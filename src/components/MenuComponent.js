3218ae44b61aa4487f6a559e94df6a09494cdba4
thegreatkingbear <pporri@yonsei.ac.kr>
2018-12-06 16:47:40 +0900

    added key to div in array to mute debugger

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -18,15 +18,16 @@ class Menu extends Component {
     render() {
         const menu = this.props.dishes.map((dish) => {
             return (
-              <div  className="col-12 col-md-5 m-1">
-                <Card key={dish.id}
-                    onClick={() => this.onDishSelect(dish)}>
-                    <CardImg width="100%" src={dish.image} alt={dish.name} />
-                    <CardImgOverlay>
-                        <CardTitle>{dish.name}</CardTitle>
-                    </CardImgOverlay>
-                </Card>
-              </div>
+            //react debugger keeps complaining about key in array. that's why I added key to below div
+                <div key={dish.id} className="col-12 col-md-5 m-1">
+                    <Card key={dish.id}
+                        onClick={() => this.onDishSelect(dish)}>
+                        <CardImg width="100%" src={dish.image} alt={dish.name} />
+                        <CardImgOverlay>
+                            <CardTitle>{dish.name}</CardTitle>
+                        </CardImgOverlay>
+                    </Card>
+                </div>
             );
         });
 
