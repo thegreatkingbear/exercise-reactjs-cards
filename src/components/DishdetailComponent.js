d450db038e5b5067c907b065bd47bf080d88c0f8
thegreatkingbear <pporri@yonsei.ac.kr>
2018-12-12 22:52:57 +0900

    update MenuComponent, DishDetailComponent to functional components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,46 +1,44 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Card, CardImg, CardBody, CardTitle, CardText } from 'reactstrap';
 
-class Dishdetail extends Component {
-    // to format date in my taste
-    formatDateNitty(dateString) {
-        const date = new Date(dateString);
-        return date.toDateString('mm dd, yyyy');
-    }
+// to format date in my taste
+function formatDateNitty(dateString) {
+    const date = new Date(dateString);
+    return date.toDateString('mm dd, yyyy');
+}
 
-    render() {
-        const selectedDish = this.props.selectedDish;
-        console.log(selectedDish);
-        const comments = this.props.selectedDish.comments.map((comment) => {
-            return (
-                <div key={comment.id} className="row">
-                    <ul className="list-unstyled">
-                        <li>{comment.comment}</li>
-                        <li>--{comment.author}, {this.formatDateNitty(comment.date)}</li>
-                    </ul>
-                </div>
-            )
-        });
-
+function Dishdetail (props) {
+    const selectedDish = props.selectedDish;
+    console.log(selectedDish);
+    const comments = props.selectedDish.comments.map((comment) => {
         return (
-            <div>
-                <Card>
-                    <CardImg top src={selectedDish.image} alt={selectedDish.name} />
-                    <CardBody>
-                        <CardTitle>{selectedDish.name}</CardTitle>
-                        <CardText>{selectedDish.description}</CardText>
-                    </CardBody>
-                </Card>
-                
-                {comments &&
-                    <div className="container">
-                        <h4>Comments</h4>
-                        {comments}
-                    </div>
-                }
+            <div key={comment.id} className="row">
+                <ul className="list-unstyled">
+                    <li>{comment.comment}</li>
+                    <li>--{comment.author}, {formatDateNitty(comment.date)}</li>
+                </ul>
             </div>
-        );
-    }
+        )
+    });
+
+    return (
+        <div>
+            <Card>
+                <CardImg top src={selectedDish.image} alt={selectedDish.name} />
+                <CardBody>
+                    <CardTitle>{selectedDish.name}</CardTitle>
+                    <CardText>{selectedDish.description}</CardText>
+                </CardBody>
+            </Card>
+            
+            {comments &&
+                <div className="container">
+                    <h4>Comments</h4>
+                    {comments}
+                </div>
+            }
+        </div>
+    );
 }
 
 export default Dishdetail;
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,32 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
 
-class Menu extends Component {
-    render() {
-        const menu = this.props.dishes.map((dish) => {
-            return (
-            // react debugger keeps complaining about key in array. 
-            // that's why I added key to below div
-                <div key={dish.id} className="col-12 col-md-5 m-1">
-                    <Card
-                        onClick={() => this.props.onClick(dish.id)}>
-                        <CardImg width="100%" src={dish.image} alt={dish.name} />
-                        <CardImgOverlay>
-                            <CardTitle>{dish.name}</CardTitle>
-                        </CardImgOverlay>
-                    </Card>
-                </div>
-            );
-        });
-
+const Menu = (props) => {
+    const menu = props.dishes.map((dish) => {
         return (
-            <div className="container">
-                <div className="row">
-                    {menu}
-                </div>
+        // react debugger keeps complaining about key in array. 
+        // that's why I added key to below div
+            <div key={dish.id} className="col-12 col-md-5 m-1">
+                <Card
+                    onClick={() => props.onClick(dish.id)}>
+                    <CardImg width="100%" src={dish.image} alt={dish.name} />
+                    <CardImgOverlay>
+                        <CardTitle>{dish.name}</CardTitle>
+                    </CardImgOverlay>
+                </Card>
             </div>
         );
-    }
+    });
+
+    return (
+        <div className="container">
+            <div className="row">
+                {menu}
+            </div>
+        </div>
+    );
 }
 
 export default Menu;
