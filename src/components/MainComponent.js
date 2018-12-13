0fb9d55752b87ba7d8870b1ad9bfbd3a66e3d761
thegreatkingbear <pporri@yonsei.ac.kr>
2018-12-13 16:18:02 +0900

    update Main to solder an error

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -34,7 +34,7 @@ class Main extends Component {
 
                 <Switch>
                     <Route path='/home' component={HomePage} />
-                    <Route exact path='/menu' component={()=> <Menu dishes={this.state.dishes} />} />
+                    <Route exact path='/menu' component={()=> <Menu dishes={this.state.dishes} onClick={(dishid) => this.onDishSelect(dishid)}/>} />
                     <Redirect to="/home" />
                 </Switch>
 
