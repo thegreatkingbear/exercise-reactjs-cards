4c06b2da0e9f550b72b735c1327893794c25a354
thegreatkingbear <pporri@yonsei.ac.kr>
2018-12-18 11:19:44 +0900

    SPA part 1

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,11 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle } from 'reactstrap';
 
-function Home(props) {
-    return (
-        <div className="container">
-            <h4>여기가 홈이라네</h4>
-        </div>
-    );
+class Home extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            dish: props.dish,
+            promotion: props.promotion,
+            leader: props.leader
+        };
+    }
+
+    render() {
+        const RenderCard = ({item}) => {
+            return (
+                <Card>
+                    <CardImg src={item.image} alt={item.name} />
+                    <CardBody>
+                        <CardTitle>{item.name}</CardTitle>
+                        {item.designation && 
+                        <CardSubtitle>{item.designation}</CardSubtitle>}
+                        <CardText>{item.description}</CardText>
+                    </CardBody>
+                </Card>
+            );
+        };
+
+        return(
+            <div className="container">
+                <div className="row align-items-start">
+                    <div className="col-12 col-md m-1">
+                        <RenderCard item={this.state.dish} />
+                    </div>
+                    <div className="col-12 col-md m-1">
+                        <RenderCard item={this.state.promotion} />
+                    </div>
+                    <div className="col-12 col-md m-1">
+                        <RenderCard item={this.state.leader} />
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Home;
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,9 +2,13 @@ import React, { Component } from 'react';
 import Menu from './MenuComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
+import Contact from './ContactComponent';
 
 class Main extends Component {
 
@@ -12,6 +16,9 @@ class Main extends Component {
         super(props);
         this.state = {
             dishes: DISHES,
+            comments: COMMENTS,
+            promotions: PROMOTIONS,
+            leaders: LEADERS,
             selectedDishId: null
         };
     }
@@ -24,7 +31,11 @@ class Main extends Component {
     render() {
         const HomePage = () => {
             return(
-                <Home />
+                <Home 
+                    dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+                    promotion={this.state.promotions.filter((promotion) => promotion.featured)[0]}
+                    leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+                />
             );
         }
 
@@ -35,6 +46,7 @@ class Main extends Component {
                 <Switch>
                     <Route path='/home' component={HomePage} />
                     <Route exact path='/menu' component={()=> <Menu dishes={this.state.dishes} onClick={(dishid) => this.onDishSelect(dishid)}/>} />
+                    <Route exact path='/contactus' component={Contact} />
                     <Redirect to="/home" />
                 </Switch>
 
