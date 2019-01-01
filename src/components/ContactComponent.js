e4b8bf26446dce0270436ae52892964e85bb8bb2
thegreatkingbear <pporri@yonsei.ac.kr>
2019-01-02 00:40:58 +0900

    update ContactComponent to reflect react redux form validator

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -3,6 +3,12 @@ import { Breadcrumb, BreadcrumbItem, Button, Label, Col, Row } from 'reactstrap'
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 
+const required = (val) => val && val.length;
+const maxLength = (len) => (val) => !(val) || (val.length <= len);
+const minLength = (len) => (val) => val && (val.length >= len);
+const isNumber = (val) => !isNaN(Number(val));
+const validEmail = (val) => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(val);
+
 class Contact extends Component {
     constructor(props) {
         super(props);
@@ -66,6 +72,21 @@ class Contact extends Component {
                                         name="firstname" 
                                         placeholder="Type your first name" 
                                         className="form-control"
+                                        validators={{
+                                            required,
+                                            minLength: minLength(3),
+                                            maxLength: maxLength(15)
+                                        }}
+                                    />
+                                    <Errors 
+                                        className="text-danger"
+                                        model=".firstname"
+                                        show="touched"
+                                        messages={{
+                                            required: "Required. ",
+                                            minLength: "3 글자 이상. ",
+                                            maxLength: "15 글자 이하. "
+                                        }}
                                     />
                                 </Col>
                             </Row>
@@ -78,6 +99,21 @@ class Contact extends Component {
                                         name="lastname" 
                                         placeholder="Type your last name" 
                                         className="form-control"
+                                        validators={{
+                                            required,
+                                            minLength: minLength(3),
+                                            maxLength: maxLength(15)
+                                        }}
+                                    />
+                                    <Errors 
+                                        className="text-danger"
+                                        model=".lastname"
+                                        show="touched"
+                                        messages={{
+                                            required: "Required. ",
+                                            minLength: "3 글자 이상. ",
+                                            maxLength: "15 글자 이하. "
+                                        }}
                                     />
                                 </Col>
                             </Row>
@@ -90,6 +126,23 @@ class Contact extends Component {
                                         name="telnum" 
                                         placeholder="Your telephone number" 
                                         className="form-control"
+                                        validators={{
+                                            required,
+                                            minLength: minLength(3),
+                                            maxLength: maxLength(15),
+                                            isNumber
+                                        }}
+                                    />
+                                    <Errors 
+                                        className="text-danger"
+                                        model=".telnum"
+                                        show="touched"
+                                        messages={{
+                                            required: "Required. ",
+                                            minLength: "3 글자 이상. ",
+                                            maxLength: "15 글자 이하. ",
+                                            isNumber: "숫자로만. "
+                                        }}
                                     />
                                 </Col>
                             </Row>
@@ -102,6 +155,19 @@ class Contact extends Component {
                                         name="email" 
                                         placeholder="Your email address here" 
                                         className="form-control"
+                                        validators={{
+                                            required,
+                                            validEmail
+                                        }}
+                                    />
+                                    <Errors 
+                                        className="text-danger"
+                                        model=".email"
+                                        show="touched"
+                                        messages={{
+                                            required: "Required. ",
+                                            validEmail: "이메일이라니까. "
+                                        }}
                                     />
                                 </Col>
                             </Row>
