6fd47fa6ce6c7cd816960e5a88632d6d4a2a3e4e
thegreatkingbear <pporri@yonsei.ac.kr>
2019-01-03 04:26:27 +0900

    task 1~3

---
+++ src/components/CommentFormComponent.js
@@ -0,0 +1,133 @@
+import React, { Component } from 'react';
+import { Button, Modal, ModalHeader, ModalBody, Label, Col, Row } from 'reactstrap';
+import { Control, LocalForm, Errors } from 'react-redux-form';
+
+const required = (val) => val && val.length;
+const maxLength = (len) => (val) => !(val) || (val.length <= len);
+const minLength = (len) => (val) => val && (val.length >= len);
+
+class CommentForm extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isOpen: props.isOpen
+        };
+
+        this.toggleModal = this.toggleModal.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    toggleModal() {
+        this.setState({
+            isOpen: !this.state.isOpen
+        })
+    }
+
+    handleSubmit(e) {
+        this.toggleModal();
+        alert(
+            "Rating : "  + JSON.stringify(e)
+        );
+    }
+
+    render() {
+        return (
+            <div>
+                <Button onClick={this.toggleModal}>
+                    Submit Comment
+                </Button>
+
+                <Modal 
+                    isOpen={this.state.isOpen} 
+                    toggle={this.toggleModal}
+                >
+                    <ModalHeader toggle={this.toggleModal}>
+                        Submit Comment
+                    </ModalHeader>
+                    <ModalBody>
+                        <LocalForm onSubmit={this.handleSubmit}>
+                            <Row className="form-group">
+                                <Label>Rating</Label>
+                                <Col>
+                                    <Control.select 
+                                        model=".rating"
+                                        id="rating"
+                                        name="rating"
+                                        className="form-control"
+                                        defaultValue="5"
+                                    >
+                                        <option>1</option>
+                                        <option>2</option>
+                                        <option>3</option>
+                                        <option>4</option>
+                                        <option>5</option>
+                                    </Control.select>
+                                </Col>
+                            </Row>
+                            <Row className="form-group">
+                                <Label>Your name</Label>
+                                <Col>
+                                    <Control.text
+                                        model=".name"
+                                        id="name"
+                                        placeholder="your name here"
+                                        className="form-control"
+                                        validators={{
+                                            required,
+                                            minLength: minLength(3),
+                                            maxLength: maxLength(10)
+                                        }}
+                                    />
+                                    <Errors 
+                                        className="text-danger"
+                                        model=".name"
+                                        show="touched"
+                                        messages={{
+                                            required: "필요해. ",
+                                            minLength: "세글자이상. ",
+                                            maxLength: "10글자 이하. "
+                                        }}
+                                    />
+                                </Col>
+                            </Row>
+                            <Row className="form-group">
+                                <Label>Comment</Label>
+                                <Col>
+                                    <Control.textarea
+                                        model=".comment"
+                                        id="comment"
+                                        placeholder="type comments"
+                                        className="form-control"
+                                        validators={{
+                                            required
+                                        }}
+                                    />
+                                    <Errors
+                                        className="text-danger"
+                                        model=".comment"
+                                        show="touched"
+                                        messages={{
+                                            required: "내용을 넣으셔야 합니다. "
+                                        }}
+                                    />
+                                </Col>
+                            </Row>
+                            <Row className="form-group">
+                                <Col>
+                                    <Button
+                                        type="submit"
+                                    >
+                                        Submit
+                                    </Button>
+                                </Col>
+                            </Row>
+                        </LocalForm>
+                    </ModalBody>
+                </Modal>
+            </div>
+        );
+    }
+}
+
+export default CommentForm;
--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Card, CardImg, CardBody, CardTitle, CardText, Breadcrumb, BreadcrumbItem } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import CommentForm from './CommentFormComponent';
 
 // to format date in my taste
 function formatDateNitty(dateString) {
@@ -8,58 +9,71 @@ function formatDateNitty(dateString) {
     return date.toDateString('mm dd, yyyy');
 }
 
-function Dishdetail (props) {
-    const selectedDish = props.selectedDish;
-    console.log(selectedDish);
-    const comments = props.comments.map((comment) => {
-        return (
-            <div key={comment.id} className="row">
-                <ul className="list-unstyled">
-                    <li>{comment.comment}</li>
-                    <li>--{comment.author}, {formatDateNitty(comment.date)}</li>
-                </ul>
-            </div>
-        )
-    });
+class Dishdetail extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedDish: props.selectedDish,
+            comments: props.comments,
+            submitComment: false
+        };
+    }
 
-    return (
-        <div className="container">
-            <div className="row">
-                <Breadcrumb>
-                    <BreadcrumbItem>
-                        <Link to="/menu">Menu</Link>
-                    </BreadcrumbItem>
-                    <BreadcrumbItem active>
-                        {selectedDish.name}
-                    </BreadcrumbItem>
-                </Breadcrumb>
+    render() {
+        const renderComments = this.state.comments.map((comment) => {
+            return (
+                <div key={comment.id} className="row">
+                    <ul className="list-unstyled">
+                        <li>{comment.comment}</li>
+                        <li>--{comment.author}, {formatDateNitty(comment.date)}</li>
+                    </ul>
+                </div>
+            )
+        });
+    
+        return (
+            <div className="container">
+                <div className="row">
+                    <Breadcrumb>
+                        <BreadcrumbItem>
+                            <Link to="/menu">Menu</Link>
+                        </BreadcrumbItem>
+                        <BreadcrumbItem active>
+                            {this.state.selectedDish.name}
+                        </BreadcrumbItem>
+                    </Breadcrumb>
+                </div>
                 <div className="col-12">
-                    <h3>{selectedDish.name}</h3>
+                    <h3>{this.state.selectedDish.name}</h3>
                     <hr />
                 </div>
-            </div>
-            <div className="row">
-                <div className="col-12 col-md-5 m-1">
-                    <Card>
-                        <CardImg top src={selectedDish.image} alt={selectedDish.name} />
-                        <CardBody>
-                            <CardTitle>{selectedDish.name}</CardTitle>
-                            <CardText>{selectedDish.description}</CardText>
-                        </CardBody>
-                    </Card>
-                </div>
-                
-                <div className="col-12 col-md-5 m-1">
-                    {comments &&
-                        <div className="container">
-                            <h4>Comments</h4>
-                            {comments}
+                <div className="row">
+                    <div className="col-12 col-md-5 m-1">
+                        <Card>
+                            <CardImg top src={this.state.selectedDish.image} alt={this.state.selectedDish.name} />
+                            <CardBody>
+                                <CardTitle>{this.state.selectedDish.name}</CardTitle>
+                                <CardText>{this.state.selectedDish.description}</CardText>
+                            </CardBody>
+                        </Card>
+                    </div>
+                    
+                    <div className="col">
+                        <div className="col-12 col-md-5 m-1">
+                            {renderComments &&
+                                <div className="container">
+                                    <h4>Comments</h4>
+                                    {renderComments}
+                                </div>
+                            }
                         </div>
-                    }
+
+                        <CommentForm isOpen={this.state.submitComment} />
+                    </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default Dishdetail;
