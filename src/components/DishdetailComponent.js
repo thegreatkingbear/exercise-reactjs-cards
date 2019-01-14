7dcd8509b23ef9fa52d8a3777f5729ac36e27467
thegreatkingbear <pporri@yonsei.ac.kr>
2019-01-14 11:32:56 -0300

    react animation components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Card, CardImg, CardBody, CardTitle, CardText, Breadcrumb, BreadcrumbItem } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
+
 import CommentForm from './CommentFormComponent';
 import Comments from './CommentsComponent';
 import Loading from './LoadingComponent';
@@ -53,17 +55,24 @@ class Dishdetail extends Component {
                     </div>
                     <div className="row">
                         <div className="col-12 col-md-5 m-1">
-                            <Card>
-                                <CardImg 
-                                    top 
-                                    src={baseUrl + this.state.selectedDish.image} 
-                                    alt={this.state.selectedDish.name} 
-                                />
-                                <CardBody>
-                                    <CardTitle>{this.state.selectedDish.name}</CardTitle>
-                                    <CardText>{this.state.selectedDish.description}</CardText>
-                                </CardBody>
-                            </Card>
+                            <FadeTransform
+                                in
+                                transformProps={{
+                                    exitTransform: 'scale(0.5) translateY(-50%)'
+                                }}
+                            >
+                                <Card>
+                                    <CardImg 
+                                        top 
+                                        src={baseUrl + this.state.selectedDish.image} 
+                                        alt={this.state.selectedDish.name} 
+                                    />
+                                    <CardBody>
+                                        <CardTitle>{this.state.selectedDish.name}</CardTitle>
+                                        <CardText>{this.state.selectedDish.description}</CardText>
+                                    </CardBody>
+                                </Card>
+                            </FadeTransform>
                         </div>
                         
                         <div className="col">
@@ -74,11 +83,20 @@ class Dishdetail extends Component {
                                     <div className="col-12 col-md-5 m-1">
                                         <div className="container">
                                             <h4>Comments</h4>
-                                            <Comments comments={this.props.comments} postComment={this.props.postComment} dishId={this.props.dishId} />
+                                            <Fade in>
+                                                <Comments 
+                                                    comments={this.props.comments} 
+                                                    postComment={this.props.postComment} 
+                                                    dishId={this.props.dishId} 
+                                                />
+                                            </Fade>
                                         </div>
                                     </div>
             
-                                    <CommentForm dishId={this.props.dishId} postComment={this.props.postComment} />
+                                    <CommentForm 
+                                        dishId={this.props.dishId} 
+                                        postComment={this.props.postComment} 
+                                    />
                                 </div>
                             }
                         </div>
--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle } from 'reactstrap';
+import { FadeTransform } from 'react-animation-components';
+
 import Loading from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
 
@@ -28,15 +30,21 @@ class Home extends Component {
             }
             else {
                 return (
-                    <Card>
-                        <CardImg src={baseUrl + item.image} alt={item.name} />
-                        <CardBody>
-                            <CardTitle>{item.name}</CardTitle>
-                            {item.designation && 
-                            <CardSubtitle>{item.designation}</CardSubtitle>}
-                            <CardText>{item.description}</CardText>
-                        </CardBody>
-                    </Card>
+                    <FadeTransform
+                        in
+                        transformProps={{
+                            exitTransform: 'scale(0.5) translateY(-50%)'
+                    }}>
+                        <Card>
+                            <CardImg src={baseUrl + item.image} alt={item.name} />
+                            <CardBody>
+                                <CardTitle>{item.name}</CardTitle>
+                                {item.designation && 
+                                <CardSubtitle>{item.designation}</CardSubtitle>}
+                                <CardText>{item.description}</CardText>
+                            </CardBody>
+                        </Card>
+                    </FadeTransform>
                 );
             }
         };
